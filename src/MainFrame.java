ee8c7be5ecfd578f63f13a7902c00920e42c23cb
SY-GG <sy.park.HUB@gmail.com>
2020-04-20 17:13:35 +0900

    Add ChangePanel, modify layout

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -1,35 +1,15 @@
-import javax.imageio.ImageIO;
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
-import javax.swing.border.LineBorder;
-import javax.swing.border.TitledBorder;
 import java.awt.*;
-
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
 import java.util.Scanner;
 
 public class MainFrame  extends JFrame {
-//
-//    private BufferedImage img01, img02, img03;
-//
-//    public void ImagePanel() {
-//        try {
-//            img01 = ImageIO.read(new File("icon1.png"));
-//            img02 = ImageIO.read(new File("icon2.png"));
-//            img03 = ImageIO.read(new File("icon3.png"));
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//    }
-
 
     //초기화, 설정
     public void home() {
         this.setTitle("자판기");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setSize(420,500);
+        this.setSize(450,520);
         this.setResizable(false);
         Dimension frameSize = this.getSize();
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
@@ -57,81 +37,103 @@ public class MainFrame  extends JFrame {
 
         //아이템, 개수, 추가 (1)
         JPanel itemPanel01 = new JPanel();
-        itemPanel01.setLayout(new GridLayout(0,2));
+        itemPanel01.setLayout(new BorderLayout());
 
         JTextField itemText01 = new JTextField("개수(1)", 5);
-        itemPanel01.add(itemText01);
-
-        itemPanel01.add(new JButton("추가"));
+        itemPanel01.add(itemText01, BorderLayout.CENTER);
+        itemPanel01.add(new JLabel("50원"), BorderLayout.NORTH);
+        itemPanel01.add(new JButton("추가"),  BorderLayout.EAST);
         entire.add(itemPanel01);
 
 
         //아이템, 개수, 추가 (2)
         JPanel itemPanel02 = new JPanel();
-        itemPanel02.setLayout(new GridLayout(0,2));
+        itemPanel02.setLayout(new BorderLayout());
 
         JTextField itemText02 = new JTextField("개수(2)", 5);
-        itemPanel02.add(itemText02);
-
-        itemPanel02.add(new JButton("추가"));
+        itemPanel02.add(itemText02, BorderLayout.CENTER);
+        itemPanel02.add(new JLabel("100원"), BorderLayout.NORTH);
+        itemPanel02.add(new JButton("추가"),  BorderLayout.EAST);
         entire.add(itemPanel02);
 
 
         //아이템, 개수, 추가 (3)
         JPanel itemPanel03 = new JPanel();
-        itemPanel03.setLayout(new GridLayout(0,2));
+        itemPanel03.setLayout(new BorderLayout());
 
         JTextField itemText03 = new JTextField("개수(3)", 5);
-        itemPanel03.add(itemText03);
+        itemPanel03.add(itemText03, BorderLayout.CENTER);
+        itemPanel03.add(new JLabel("200원"), BorderLayout.NORTH);
+        itemPanel03.add(new JButton("추가"),  BorderLayout.EAST);
+        entire.add(itemPanel03);
 
-        itemPanel03.add(new JButton("추가"));
         entire.add(itemPanel03);
 
 
-        //현재 금액, 시작
+        //현재 금액
         JPanel BuyPanel = new JPanel();
-        BuyPanel.setLayout(new BorderLayout(10,10));
+        BuyPanel.setLayout(new BorderLayout(5,10));
         BuyPanel.setBorder(new EmptyBorder(10, 10, 10, 10));
         BuyPanel.setBackground(Color.WHITE);
 
-        JLabel buyLabel = new JLabel("합계"); //합계 텍스트, 정렬
+        JLabel buyLabel = new JLabel("투입금"); //투입금 텍스트
         buyLabel.setFont(new Font("맑은 고딕", Font.BOLD, 30));
         buyLabel.setHorizontalAlignment(JLabel.CENTER);
         BuyPanel.add(buyLabel, BorderLayout.NORTH);
 
-        JTextField buyText = new JTextField("현재 금액",15);
+        JTextField buyText = new JTextField("현재 금액",10);
         BuyPanel.add(buyText, BorderLayout.CENTER);
         BuyPanel.add(new JButton("시작"),BorderLayout.EAST);
+
         entire.add(BuyPanel);
 
 
-        //합계, 구매
+        //구매
         JPanel PayPanel = new JPanel();
-        PayPanel.setLayout(new BorderLayout(10,10));
+        PayPanel.setLayout(new BorderLayout(5,10));
         PayPanel.setBorder(new EmptyBorder(10, 10, 10, 10));
         PayPanel.setBackground(Color.WHITE);
 
-        JLabel payLabel = new JLabel("합계"); //합계 텍스트, 정렬
+        JLabel payLabel = new JLabel("합계금"); //텍스트, 정렬
         payLabel.setFont(new Font("맑은 고딕", Font.BOLD, 30));
         payLabel.setHorizontalAlignment(JLabel.CENTER);
         PayPanel.add(payLabel, BorderLayout.NORTH);
 
-        JTextField payText = new JTextField("총 금액",15);
+        JTextField payText = new JTextField("총 금액",10);
         PayPanel.add(payText, BorderLayout.CENTER);
         PayPanel.add(new JButton("구매"), BorderLayout.EAST);
+
         entire.add(PayPanel);
 
 
-//        JPanel CoinPanel = new JPanel(); // 거스름돈 동전
-//        entire.add(CoinPanel);
-//            CoinPanel.setBackground(Color.BLACK);
+        //거스름돈 동전
+        JPanel ChangePanel = new JPanel(new BorderLayout(5,10));
+        ChangePanel.setBorder(new EmptyBorder(10, 10, 10, 10));
 
+        JLabel changeLabel = new JLabel("거스름돈"); //텍스트, 정렬
+        changeLabel.setFont(new Font("맑은 고딕", Font.BOLD, 30));
+        changeLabel.setHorizontalAlignment(JLabel.CENTER);
+        ChangePanel.add(changeLabel, BorderLayout.NORTH);
 
+        JPanel CoinPanel = new JPanel(new GridLayout(2,5, 6,3));
 
+        String coinValue[] = {"10", "50", "100", "160", "500"};
+        for(int i = 0; i<5; i++){CoinPanel.add(new JLabel(coinValue[i]+"원", JLabel.CENTER));}
 
+        JTextField coin10 = new JTextField("0개",3);
+        JTextField coin50 = new JTextField("0개",3);
+        JTextField coin100 = new JTextField("0개",3);
+        JTextField coin160 = new JTextField("0개",3);
+        JTextField coin500 = new JTextField("0개",3);
+        CoinPanel.add (coin10); CoinPanel.add (coin50); CoinPanel.add (coin100); CoinPanel.add (coin160); CoinPanel.add (coin500);
+
+        ChangePanel.add(CoinPanel, BorderLayout.CENTER);
+        entire.add(ChangePanel);
 
         this.setVisible(true);
     }
+
+
     public static class item{
         String name;
         int cost;
